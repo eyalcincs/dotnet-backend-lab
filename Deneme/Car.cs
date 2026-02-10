2aa48e3ae8dd50d4d9c0af3bc92d0115ac9ae335
rebellious14 <121296276+rebellious14@users.noreply.github.com>
2026-02-10 03:27:17 +0300

    Generic Class

--- Deneme/Car.cs
+++ Deneme/Car.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace Deneme
 {
-    public class Car
+    public class Car<T>
     {
+        T[] items;
+
+        // Constructor
+        public Car() {
+
+            items = new T[0];
+        }
 
 
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[tempArray.Length];
 
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+
+        }
     } 
 }
